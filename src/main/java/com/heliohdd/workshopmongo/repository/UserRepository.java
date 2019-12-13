6e3b1b8b004bfc8a4809fb0356d42a318e0c6bd3
Helio Dourado <heliohdd@yahoo.com.br>
2019-12-13 18:27:57 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/heliohdd/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.heliohdd.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.heliohdd.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+	
+	
+	
+
+}
