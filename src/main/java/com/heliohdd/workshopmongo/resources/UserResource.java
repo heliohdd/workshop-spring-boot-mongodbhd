90e2c375635b9e59be90927b64a2729dba340527
Helio Dourado <heliohdd@yahoo.com.br>
2019-12-15 08:55:15 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/heliohdd/workshopmongo/resources/UserResource.java
+++ src/main/java/com/heliohdd/workshopmongo/resources/UserResource.java
@@ -1,8 +1,7 @@
 package com.heliohdd.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.heliohdd.workshopmongo.domain.User;
+import com.heliohdd.workshopmongo.dto.UserDTO;
 import com.heliohdd.workshopmongo.services.UserService;
 
 @RestController
@@ -22,9 +22,10 @@ public class UserResource {
 	
 //	@RequestMapping(method = RequestMethod.GET)		//	-> Método alternativo
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
