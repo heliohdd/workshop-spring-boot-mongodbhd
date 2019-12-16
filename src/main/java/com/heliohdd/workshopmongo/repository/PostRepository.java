d3021612761aea7e185804c39aed1b8f9e85808f
Helio Dourado <heliohdd@yahoo.com.br>
2019-12-16 14:55:56 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/heliohdd/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.heliohdd.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.heliohdd.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+	
+
+}
