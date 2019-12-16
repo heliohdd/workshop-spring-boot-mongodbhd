fde21298f64a9abdcc5df12f2a9526a50d198b0c
Helio Dourado <heliohdd@yahoo.com.br>
2019-12-16 15:20:32 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/heliohdd/workshopmongo/config/Instantiation.java
+++ src/main/java/com/heliohdd/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.heliohdd.workshopmongo.domain.Post;
 import com.heliohdd.workshopmongo.domain.User;
+import com.heliohdd.workshopmongo.dto.AuthorDTO;
 import com.heliohdd.workshopmongo.repository.PostRepository;
 import com.heliohdd.workshopmongo.repository.UserRepository;
 
@@ -37,8 +38,8 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postrepository.saveAll(Arrays.asList(post1, post2));
 	}
